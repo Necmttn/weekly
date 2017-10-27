79b1f0fd267d99b7863ca831ae854473dedd436e
Necmettin Karakaya <necmettin.karakaya@gmail.com>
2017-10-27 21:16:27 +0800

    setup base plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,72 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+     {
+       resolve: 'gatsby-source-trello',
+       options: {
+         teamId: `thebaseglobal`,
+         apiKey: process.env.TRELLO_KEY,
+         secret: process.env.TRELLO_SECRET
+       }
+     },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-responsive-image',
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyForNull: 'any',
+        langKeyDefault: 'en',
+        useLangKeyLayout: false,
+        markdownRemark: {
+          postPage: 'src/templates/blog-post.js',
+          query: `
+          {
+              allMarkdownRemark {
+                  edges {
+                  node {
+                      fields {
+                      slug,
+                      langKey
+                      }
+                  }
+                  }
+              }
+          }
+          `
+        }
+      }
+    },
+    `gatsby-plugin-styled-components`
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    weekly is a place for programmers of all backgrounds to learn and share ideas.
+    don't forget to subscribe our weekly mail.
   </div>
 )
 
